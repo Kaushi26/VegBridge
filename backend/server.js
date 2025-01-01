a9af4f212c821dc2cb3e95d66ef60fda064af189
Kaushick N <143161163+Kaushi26@users.noreply.github.com>
2025-01-01 14:29:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // CORS options
 const corsOptions = {
-  origin: `${process.env.FRONTEND_URL}`,
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
